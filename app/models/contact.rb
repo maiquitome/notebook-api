72723abc409e621f8cd244584a913d93af82335e
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2021-07-12 08:54:13 -0300

    :sparkles: using post with associations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,25 @@
 class Contact < ApplicationRecord
   belongs_to :kind
+  # belongs_to :kind, optional: true
 
   def author
     "Maiqui Tomé"
   end
 
+  def kind_description
+    self.kind.description
+  end
+
   # definindo padrão pra todos (redefenindo o método)
   def as_json(options = {})
     # o super invoca o as_json original
-    super(methods: :author, root: true)
+    super(
+      root: true,
+      methods: [:kind_description, :author],
+
+      # methods: :author,
+      # include: :kind
+      # include: { kind: {only: :description} }
+    )
   end
 end
